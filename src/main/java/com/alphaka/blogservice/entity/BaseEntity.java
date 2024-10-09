ed35587493571e2a64fbda01b3d82d39c9fe9300
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-09 23:49:34 +0900

    AK-116 [Remove] baseentity에서 삭제 정보 관리 필드 제거

--- src/main/java/com/alphaka/blogservice/entity/BaseEntity.java
+++ src/main/java/com/alphaka/blogservice/entity/BaseEntity.java
@@ -14,11 +14,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
-    //생성 시간과 업데이트 시간을 알려주는 부모 클래스
-
-    // 특징:
-    // 따로 테이블을 생성하지 않음
-    // 대신 이 클래스의 필드들은 하위 엔티티에 포함됨
 
     @CreatedDate
     @Column(updatable = false, nullable = false)
@@ -29,13 +24,4 @@ public abstract class BaseEntity {
     @LastModifiedDate
     @Column(insertable = false)
     private LocalDateTime updatedAt;
-
-    // null 값 존재 가능
-    // 삭제 되면 삭제 시간 직접 기록
-    private LocalDateTime deletedAt;
-
-    // 논리적인 삭제 구현
-    public void markAsDeleted() {
-        this.deletedAt = LocalDateTime.now();
-    }
 }
