a3d764d5ae20eff8eec39647d56d76cf07dc85f5
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-09 19:59:18 +0900

    AK-110 [Feat] Kafka 사용자 생성 이벤트 수신 및 블로그 생성 호출 기능

---
+++ src/main/java/com/alphaka/blogservice/service/EventListenerService.java
@@ -0,0 +1,43 @@
+package com.alphaka.blogservice.service;
+
+import com.alphaka.blogservice.client.UserClient;
+import com.alphaka.blogservice.dto.UserSignUpEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class EventListenerService {
+
+    private final BlogService blogService;
+    private final UserClient userClient;
+
+    // Kafka Topic으로부터 UserSignUpEvent를 수신하여 블로그 생성
+    @Transactional
+    @KafkaListener(topics = "user-signup", groupId = "blog-service")
+    public void handleUserSignUpEvent(UserSignUpEvent userSignUpEvent) {
+        log.info("UserSignUpEvent received: {}", userSignUpEvent);
+
+        // 유저 존재 여부 검증 (boolean 사용)
+        log.info("Checking user existence for ID: {}", userSignUpEvent.getId());
+        if (!userClient.isUserExists(userSignUpEvent.getId())) {
+            log.error("User not found for ID: {}", userSignUpEvent.getId());
+            //throw new UserNotFoundException("User not found with ID: " + userSignUpEvent.getId());
+        }
+        log.info("User found for ID: {}", userSignUpEvent.getId());
+
+        // 블로그 생성 (실패시 롤백)
+        try {
+            blogService.createBlog(userSignUpEvent.getId());
+            log.info("Blog created for user: {}", userSignUpEvent.getId());
+        } catch (Exception e) {
+            log.error("Blog creation failed: {}", e.getMessage());
+            //throw new BlogCreationFailedException("Blog creation failed: " + e.getMessage());
+        }
+    }
+}
