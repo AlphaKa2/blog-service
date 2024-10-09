c61056ff7af9bff3f229769ee97384a09dbde4aa
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-09 22:26:05 +0900

    AK-113 [Feat] 일관성 있는 오류 응답을 반환할 객체 추가

---
+++ src/main/java/com/alphaka/blogservice/dto/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.alphaka.blogservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String code;
+    private String message;
+}
