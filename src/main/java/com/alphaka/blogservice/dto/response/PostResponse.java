1a151bc65a7fcc5b2d3b8916f6c2f31fe6bde149
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-11 19:53:49 +0900

    AK-121 [Feat] 블로그 ID 필드 추가

--- src/main/java/com/alphaka/blogservice/dto/response/PostResponse.java
+++ src/main/java/com/alphaka/blogservice/dto/response/PostResponse.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostResponse {
 
+    private Long blogId;
     private Long userId;
     private String title;
     private String content;
