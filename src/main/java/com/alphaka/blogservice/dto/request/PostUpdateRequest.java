201f564d88caa19fa2ab768a98ad27a404ae2c97
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-11 19:54:17 +0900

    AK-121 [Fix] 엔티티와 공개여부 필드명 통일

--- src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
+++ src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
@@ -23,7 +23,7 @@ public class PostUpdateRequest {
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
 
-    private boolean isPrivate;
+    private boolean isPublic;
     private boolean isCommentable;
 
     private List<MultipartFile> images;
