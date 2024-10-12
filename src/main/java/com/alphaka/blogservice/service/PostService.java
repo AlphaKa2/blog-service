9e1db9e42b467667e2ebfad42dbc6bcab972c294
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-12 18:43:02 +0900

    AK-132 [Feat] 사용자 인증정보 추출 기능 분리 및 미디어 파일 처리 통합

--- src/main/java/com/alphaka/blogservice/service/PostService.java
+++ src/main/java/com/alphaka/blogservice/service/PostService.java
@@ -1,10 +1,12 @@
 package com.alphaka.blogservice.service;
 
 import com.alphaka.blogservice.Mapper.PostMapper;
-import com.alphaka.blogservice.client.AuthClient;
+import com.alphaka.blogservice.client.UserClient;
 import com.alphaka.blogservice.dto.request.PostCreateRequest;
 import com.alphaka.blogservice.dto.request.PostUpdateRequest;
+import com.alphaka.blogservice.dto.request.UserProfile;
 import com.alphaka.blogservice.dto.response.PostResponse;
+import com.alphaka.blogservice.entity.Blog;
 import com.alphaka.blogservice.entity.Post;
 import com.alphaka.blogservice.exception.custom.BlogNotFoundException;
 import com.alphaka.blogservice.exception.custom.PostNotFoundException;
@@ -29,27 +31,34 @@ public class PostService {
 
     private final S3Service s3Service;
     private final TagService tagService;
+    private final UserClient userClient;
+    private final UserProfileService userProfileService;
     private final BlogRepository blogRepository;
     private final PostRepository postRepository;
     private final PostMapper postMapper = PostMapper.INSTANCE;
 
     /**
      * 게시글 작성
-     * @param httpRequest HttpServletRequest
-     * @param request PostCreateRequest
-     * @return PostResponse
+     * @param httpRequest HTTP 요청
+     * @param request 작성할 게시글 정보
+     * @return PostResponse 작성한 게시글 정보
      */
     @Transactional
     public PostResponse createPost(HttpServletRequest httpRequest, PostCreateRequest request) {
-        log.info("게시글 작성 요청 - Blog ID: {}, Title: {}", request.getBlogId(), request.getTitle());
+        log.info("게시글 작성 요청 - Nickname: {}, Title: {}", request.getNickname(), request.getTitle());
 
-        Long userId = getAuthenticatedUserId(httpRequest);  // 사용자 ID 추출 및 확인
+        // 현재 사용자 정보 추출
+        UserProfile userProfile = userProfileService.getUserProfileFromHeader(httpRequest);
 
+        // 닉네임을 통해 블로그 조회
+        Blog blog = blogRepository.findById(userProfile.getUserId()).orElseThrow(BlogNotFoundException::new);
+
+        // 게시글 내용 처리
         String processedContent = processFiles(request.getContent(), request.getImages(), request.getVideos());
 
         Post post = Post.builder()
-                .userId(userId)
-                .blog(blogRepository.findById(request.getBlogId()).orElseThrow(BlogNotFoundException::new))
+                .userId(userProfile.getUserId())
+                .blog(blog)
                 .title(request.getTitle())
                 .content(processedContent)
                 .isPublic(request.isPublic())
@@ -59,7 +68,7 @@ public class PostService {
         postRepository.save(post);
         log.info("게시글 작성 완료 - Post ID: {}", post.getId());
 
-        // 태그 업데이트
+        // 태그 처리
         tagService.updateTagsForPost(post, request.getTagNames());
 
         return postMapper.toResponse(post);
@@ -67,21 +76,26 @@ public class PostService {
 
     /**
      * 게시글 수정
-     * @param httpRequest HttpServletRequest
+     * @param httpRequest HTTP 요청
      * @param postId 게시글 ID
-     * @param request PostUpdateRequest
-     * @return PostResponse
+     * @param request 게시글 수정 정보
+     * @return PostResponse 수정한 게시글 정보
      */
     @Transactional
     public PostResponse updatePost(HttpServletRequest httpRequest, Long postId, PostUpdateRequest request) {
         log.info("게시글 수정 요청 - Post ID: {}", postId);
 
-        Long userId = getAuthenticatedUserId(httpRequest);
-        Post post = validatePostOwnership(postId, userId);  // 게시글 작성자 확인
+        // 현재 사용자 정보 추출
+        UserProfile userProfile = userProfileService.getUserProfileFromHeader(httpRequest);
 
+        // 게시글 작성자 확인
+        Post post = validatePostOwnership(postId, userProfile.getUserId());
+
+        // 게시글 내용 처리
         String processedContent = processFiles(request.getContent(), request.getImages(), request.getVideos());
 
         post.updatePost(request.getTitle(), processedContent, request.isPublic(), request.isCommentable());
+
         postRepository.save(post);
         log.info("게시글 수정 완료 - Post ID: {}", post.getId());
 
@@ -93,43 +107,18 @@ public class PostService {
 
     /**
      * 게시글 삭제
-     * @param request HttpServletRequest
+     * @param request HTTP 요청
      * @param postId 게시글 ID
      */
     @Transactional
     public void deletePost(HttpServletRequest request, Long postId) {
         log.info("게시글 삭제 요청 - Post ID: {}", postId);
 
-        Long userId = getAuthenticatedUserId(request);
-        Post post = validatePostOwnership(postId, userId);  // 게시글 작성자 확인
+        UserProfile userProfile = userProfileService.getUserProfileFromHeader(request); // 사용자 정보 추출
+        Post post = validatePostOwnership(postId, userProfile.getUserId());  // 게시글 작성자 확인
 
         postRepository.delete(post);
         log.info("게시글 삭제 완료 - Post ID: {}", post.getId());
-    }
-
-
-    /**
-     * 현재 인증된 사용자 ID를 추출하고 확인
-     * @param request HttpServletRequest
-     * @return 사용자 ID
-     */
-    private Long getAuthenticatedUserId(HttpServletRequest request) {
-        String userIdHeader = request.getHeader("X-USER-ID");
-
-        if (userIdHeader == null) {
-            log.error("헤더에서 사용자 정보를 찾을 수 없습니다.");
-            throw new UnauthorizedException();
-        }
-
-        // 사용자 ID가 숫자인지 확인
-        try {
-            Long userId = Long.parseLong(userIdHeader);
-            log.info("인증된 사용자 ID: {}", userId);
-            return userId;
-        } catch (NumberFormatException e) {
-            log.error("헤더의 사용자 ID가 유효하지 않습니다: {}", userIdHeader);
-            throw new UnauthorizedException();
-        }
     }
 
     /**
@@ -141,6 +130,7 @@ public class PostService {
     private Post validatePostOwnership(Long postId, Long userId) {
         Post post = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
 
+        // 게시글 작성자와 현재 사용자 ID가 같은지 확인
         if (!post.getUserId().equals(userId)) {
             log.error("게시글 작성자가 아닙니다 - Post ID: {}, User ID: {}", post.getId(), userId);
             throw new UnauthorizedException();
@@ -154,7 +144,7 @@ public class PostService {
      * @param content HTML 내용
      * @param images 이미지 파일 목록
      * @param videos 비디오 파일 목록
-     * @return 파일 경로가 반영된 HTML 내용
+     * @return content 파일 경로가 반영된 HTML 내용
      */
     public String processFiles(String content, List<MultipartFile> images, List<MultipartFile> videos) {
         log.info("게시글 미디어 파일 처리 시작");
@@ -168,31 +158,30 @@ public class PostService {
 
     // 이미지 파일 처리
     private String processImages(String content, List<MultipartFile> images) {
-        for (MultipartFile image : images) {
-            try {
-                String imageName = Objects.requireNonNull(image.getOriginalFilename());
-                if (content.contains(imageName)) {
-                    String imageUrl = s3Service.uploadPostImage(image);
-                    content = content.replace(imageName, imageUrl);  // HTML에서 이미지 파일명을 S3 URL로 교체
-                }
-            } catch (Exception e) {
-                log.error("이미지 처리 중 오류가 발생했습니다: {}", image.getOriginalFilename(), e);
-            }
-        }
-        return content;
+        return processMedia(content, images, "image");
     }
 
     // 비디오 파일 처리
     private String processVideos(String content, List<MultipartFile> videos) {
-        for (MultipartFile video : videos) {
+        return processMedia(content, videos, "video");
+    }
+
+    // 미디어 파일 처리
+    private String processMedia(String content, List<MultipartFile> mediaFiles, String mediaType) {
+        for (MultipartFile media : mediaFiles) {
             try {
-                String videoName = Objects.requireNonNull(video.getOriginalFilename());
-                if (content.contains(videoName)) {
-                    String videoUrl = s3Service.uploadPostVideo(video);
-                    content = content.replace(videoName, videoUrl);  // HTML에서 비디오 파일명을 S3 URL로 교체
+                String mediaName = Objects.requireNonNull(media.getOriginalFilename());
+                if (content.contains(mediaName)) {
+                    String mediaUrl;
+                    if (mediaType.equals("image")) {
+                        mediaUrl = s3Service.uploadPostImage(media);
+                    } else {
+                        mediaUrl = s3Service.uploadPostVideo(media);
+                    }
+                    content = content.replace(mediaName, mediaUrl);  // HTML에서 파일명을 S3 URL로 교체
                 }
             } catch (Exception e) {
-                log.error("비디오 처리 중 오류가 발생했습니다: {}", video.getOriginalFilename(), e);
+                log.error("{} 처리 중 오류가 발생했습니다: {}", mediaType, media.getOriginalFilename(), e);
             }
         }
         return content;
