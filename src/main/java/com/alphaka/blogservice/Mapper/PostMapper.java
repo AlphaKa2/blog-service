acfdbb85257baa88700cb4048f1f72d801a57581
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-12 18:43:25 +0900

    AK-132 [Fix] 수정한 DTO에 맞춰 매핑 변경

--- src/main/java/com/alphaka/blogservice/Mapper/PostMapper.java
+++ src/main/java/com/alphaka/blogservice/Mapper/PostMapper.java
@@ -1,8 +1,6 @@
 package com.alphaka.blogservice.Mapper;
 
-import com.alphaka.blogservice.dto.request.PostCreateRequest;
 import com.alphaka.blogservice.dto.response.PostResponse;
-import com.alphaka.blogservice.entity.Blog;
 import com.alphaka.blogservice.entity.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -16,10 +14,11 @@ public interface PostMapper {
 
     // Post -> PostResponse 매핑
     @Mappings({
-            @Mapping(source = "blog.id", target = "blogId"),  // Blog ID 매핑
-            @Mapping(source = "userId", target = "userId"),   // 작성자 ID 매핑
+            @Mapping(source = "blog.id", target = "blogId"),           // Blog ID 매핑
+            @Mapping(source = "blog.nickname", target = "nickname"),   // Blog의 닉네임 매핑
+            @Mapping(source = "userId", target = "userId"),            // 작성자 ID 매핑
             @Mapping(source = "createdAt", target = "createdAt", dateFormat = "yyyy-MM-dd HH:mm:ss"),  // 작성일시 포맷팅
-            @Mapping(source = "updatedAt", target = "updatedAt", dateFormat = "yyyy-MM-dd HH:mm:ss")  // 수정일시 포맷팅
+            @Mapping(source = "updatedAt", target = "updatedAt", dateFormat = "yyyy-MM-dd HH:mm:ss")   // 수정일시 포맷팅
     })
     PostResponse toResponse(Post post);
 }
