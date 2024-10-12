75e1f8c664609d1cbd8f09c03e04eda40f2b739d
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-12 17:17:45 +0900

    AK-132 [Fix] 블로그ID 대신 사용자 닉네임을 사용하도록 변경

--- src/main/java/com/alphaka/blogservice/dto/request/PostCreateRequest.java
+++ src/main/java/com/alphaka/blogservice/dto/request/PostCreateRequest.java
@@ -1,7 +1,7 @@
 package com.alphaka.blogservice.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,10 +14,11 @@ import java.util.List;
 @AllArgsConstructor
 public class PostCreateRequest {
 
-    @NotNull(message = "Blog ID는 필수입니다.")
-    private Long blogId;
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;  // blogId 대신 사용
 
     @NotBlank(message = "제목은 필수입니다.")
+    @Max(value = 100, message = "제목은 최대 100자까지 입력 가능합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
--- src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
+++ src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
@@ -1,9 +1,10 @@
 package com.alphaka.blogservice.dto.request;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,13 +12,15 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostUpdateRequest {
 
-    @NotNull(message = "Blog ID는 필수입니다.")
-    private Long blogId;
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;  // blogId 대신 사용
 
     @NotBlank(message = "제목은 필수입니다.")
+    @Max(value = 100, message = "제목은 최대 100자까지 입력 가능합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
