e1b7e224c85ba454d9761bc44e0cbe74356eda61
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-12 18:54:13 +0900

    AK-132 [Fix] 제거할 태그를 동일한 태그 목록에서 조회하는 계산 오류 수정 및 findOrCreateTags

--- src/main/java/com/alphaka/blogservice/repository/TagRepository.java
+++ src/main/java/com/alphaka/blogservice/repository/TagRepository.java
@@ -4,6 +4,7 @@ import com.alphaka.blogservice.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,7 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
 
     // 태그 이름으로 태그 조회
     Optional<Tag> findByTagName(String tagName);
+
+    // 태그 이름으로 태그 조회
+    List<Tag> findByTagNameIn(List<String> tagNames);
 }
