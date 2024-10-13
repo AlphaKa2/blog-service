10ccd0b5c62f18234ea758ede4bb111796074c5a
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-13 19:30:20 +0900

    AK-136 [Remove] 사용하지 않는 메서드 삭제

--- src/main/java/com/alphaka/blogservice/client/AuthClient.java
+++ src/main/java/com/alphaka/blogservice/client/AuthClient.java
@@ -1,12 +1,7 @@
 package com.alphaka.blogservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "auth-service")
 public interface AuthClient {
-
-    // 사용자 정보 추출
-    @GetMapping("/auth/extract/{userId}")
-    Long extractUserId(String userId);
 }
