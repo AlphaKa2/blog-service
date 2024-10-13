e7680b8025fac97353c4cae26f8bf6c9c23b6d53
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-13 16:50:56 +0900

    AK-136 [Feat] 게시글 상세 조회 매퍼 추가

--- src/main/java/com/alphaka/blogservice/Mapper/PostMapper.java
+++ src/main/java/com/alphaka/blogservice/Mapper/PostMapper.java
@@ -1,11 +1,14 @@
 package com.alphaka.blogservice.Mapper;
 
+import com.alphaka.blogservice.dto.response.PostDetailResponse;
 import com.alphaka.blogservice.dto.response.PostResponse;
 import com.alphaka.blogservice.entity.Post;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
@@ -27,4 +30,16 @@ public interface PostMapper {
             @Mapping(target = "tags", expression = "java(post.getPostTags().stream().map(postTag -> postTag.getTag().getTagName()).collect(java.util.stream.Collectors.toList()))") // 태그 목록 매핑
     })
     PostResponse toResponse(Post post, String nickname);
+
+    // Post -> PostDetailResponse 매핑
+    @Mappings({
+            @Mapping(source = "post.id", target = "postId"),
+            @Mapping(source = "nickname", target = "author"),
+            @Mapping(source = "post.title", target = "title"),
+            @Mapping(source = "post.content", target = "content"),
+            @Mapping(source = "tags", target = "tags"),
+            @Mapping(target = "likeCount", expression = "java(post.getLikes().size())"),
+            @Mapping(source = "post.createdAt", target = "createdAt", dateFormat = "yyyy-MM-dd HH:mm:ss")
+    })
+    PostDetailResponse toResponse(Post post, String nickname, List<String> tags);
 }
