d74a139af0a616170f06d55046e8f8412aaef871
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-13 16:51:08 +0900

    AK-136 [Feat] 게시글 상세 조회수 증가 쿼리 추가

--- src/main/java/com/alphaka/blogservice/repository/PostRepository.java
+++ src/main/java/com/alphaka/blogservice/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.alphaka.blogservice.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -19,4 +20,9 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "WHERE p.blog.id = :blogId " +
             "GROUP BY p.id")
     Page<Object[]> findPostLikeAndCommentCountsByBlogId(@Param("blogId") Long blogId, Pageable pageable);
+
+    // 게시글 조회수 증가
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Post p SET p.viewCount = p.viewCount + 1 WHERE p.id = :postId")
+    void increaseViewCount(@Param("postId") Long postId);
 }
