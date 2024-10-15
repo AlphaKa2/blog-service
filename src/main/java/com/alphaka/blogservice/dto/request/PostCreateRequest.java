9cafca1c2ed7e377cf0cf696120de7185489c389
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-15 19:03:26 +0900

    AK-169 [Refactor] 변수명 충돌로 인한 변수명 변경

--- src/main/java/com/alphaka/blogservice/dto/request/PostCreateRequest.java
+++ src/main/java/com/alphaka/blogservice/dto/request/PostCreateRequest.java
@@ -2,15 +2,14 @@ package com.alphaka.blogservice.dto.request;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PostCreateRequest {
 
@@ -24,8 +23,8 @@ public class PostCreateRequest {
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
 
-    private boolean isPublic;
-    private boolean isCommentable;
+    private boolean visible;
+    private boolean commentable;
 
     private List<MultipartFile> images;
     private List<MultipartFile> videos;
