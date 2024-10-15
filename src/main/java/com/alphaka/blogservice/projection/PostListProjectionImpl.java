14ebcaf7c2a4e9b0a53a5ce73608b98f32f63937
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-15 20:56:15 +0900

    AK-169 [Fix] 게시글 목록 프로젝션 필드 수정

--- src/main/java/com/alphaka/blogservice/projection/PostListProjectionImpl.java
+++ src/main/java/com/alphaka/blogservice/projection/PostListProjectionImpl.java
@@ -1,6 +1,5 @@
 package com.alphaka.blogservice.projection;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -13,30 +12,83 @@ import java.util.List;
  * DB에서 필요한 필드만 조회하여 반환하기 위한 프로젝션
  */
 @Setter
-@Getter
 @NoArgsConstructor
 public class PostListProjectionImpl implements PostListProjection{
     private Long postId;
     private String title;
     private String content;
-    private int likeCount;
-    private int commentCount;
-    private int viewCount;
+    private Long likeCount;
+    private Long commentCount;
+    private Integer viewCount;
+    private boolean visible;
+    private boolean commentable;
     private LocalDateTime createdAt;
     private List<String> tags;
 
-    public PostListProjectionImpl(Long postId, String title, String content, int likeCount, int commentCount,
-                                  int viewCount, LocalDateTime createdAt, String tagName) {
+    public PostListProjectionImpl(Long postId, String title, String content, Long likeCount, Long commentCount, Integer viewCount,
+                                  boolean visible, boolean commentable, LocalDateTime createdAt, String tagName) {
         this.postId = postId;
         this.title = title;
         this.content = content;
         this.likeCount = likeCount;
         this.commentCount = commentCount;
         this.viewCount = viewCount;
+        this.visible = visible;
+        this.commentable = commentable;
         this.createdAt = createdAt;
         this.tags = new ArrayList<>();
         if (!tagName.isEmpty()) {
             this.tags.add(tagName);
         }
+    }
+
+    @Override
+    public Long getPostId() {
+        return postId;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public Long getLikeCount() {
+        return likeCount;
+    }
+
+    @Override
+    public Long getCommentCount() {
+        return commentCount;
+    }
+
+    @Override
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    @Override
+    public boolean getVisible() {
+        return visible;
+    }
+
+    @Override
+    public boolean getCommentable() {
+        return commentable;
+    }
+
+    @Override
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public List<String> getTags() {
+        return tags;
     }
 }
