e17adb16a2bef4fab8d48700ee3b5708a73696e4
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-15 23:40:07 +0900

    AK-169 [Feat] 각각 부모와 자식 댓글을 별도로 조회하는 기능 추가

--- src/main/java/com/alphaka/blogservice/repository/CommentRepository.java
+++ src/main/java/com/alphaka/blogservice/repository/CommentRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
     // 특정 게시글의 부모 댓글 모두 조회
     List<Comment> findByPostAndParentIsNull(Post post);
