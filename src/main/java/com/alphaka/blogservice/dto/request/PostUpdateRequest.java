dc8a57989c8ec46e42a28c4f12db2b8ce70cfb1b
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-16 15:17:20 +0900

    AK-169 [Feat] 유효성 검사 범위 수정

--- src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
+++ src/main/java/com/alphaka/blogservice/dto/request/PostUpdateRequest.java
@@ -1,7 +1,7 @@
 package com.alphaka.blogservice.dto.request;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +19,7 @@ public class PostUpdateRequest {
     private String nickname;  // blogId 대신 사용
 
     @NotBlank(message = "제목은 필수입니다.")
-    @Max(value = 100, message = "제목은 최대 100자까지 입력 가능합니다.")
+    @Size(min = 1, max = 100, message = "제목은 1자 이상 100자 이하여야 합니다.")
     private String title;
 
     @NotBlank(message = "내용은 필수입니다.")
