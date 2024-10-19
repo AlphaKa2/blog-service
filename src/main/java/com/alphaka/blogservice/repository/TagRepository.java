83cd84580cec54e6b8e00e03613b0da647772fee
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-20 00:40:57 +0900

    AK-191 [Feat] 태그 등록 배치 삽입 기능 추가

--- src/main/java/com/alphaka/blogservice/repository/TagRepository.java
+++ src/main/java/com/alphaka/blogservice/repository/TagRepository.java
@@ -5,13 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface TagRepository extends JpaRepository<Tag, Long> {
-
-    // 태그 이름으로 태그 조회
-    Optional<Tag> findByTagName(String tagName);
+public interface TagRepository extends JpaRepository<Tag, Long>, TagRepositoryCustom {
 
     // 태그 이름으로 태그 조회
     List<Tag> findByTagNameIn(List<String> tagNames);
