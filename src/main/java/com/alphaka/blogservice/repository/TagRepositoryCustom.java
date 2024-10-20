a1844daae35c269b67cf8255c450fdfc5670b6cb
Hojun Im <ghwnsdla8094@gmail.com>
2024-10-20 15:52:23 +0900

    AK-192 [Feat] 태그 조회 기능 별도로 분리

--- src/main/java/com/alphaka/blogservice/repository/TagRepositoryCustom.java
+++ src/main/java/com/alphaka/blogservice/repository/TagRepositoryCustom.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface TagRepositoryCustom {
     // 태그 엔티티를 일괄 저장
     void batchInsert(List<Tag> tags);
+
+    // 게시글 ID로 태그 조회
+    List<String> findTagsByPostId(Long postId);
 }
